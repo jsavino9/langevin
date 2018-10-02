e0cb649c385b0b4ae4af45ae336c98ccc7632592
Jsavino <jsavino@u.rochester.edu>
2018-10-02 19:48:10 -0400

    Added histogramming and wall size arguments

--- langevin_project/langevin_project.py
+++ langevin_project/langevin_project.py
@@ -6,22 +6,26 @@ import numpy as np
 import matplotlib.pyplot as plt
 import argparse
 
-def euler(function,t0,tf,ts,f0,T,gamma):
+def euler(function,t0,tf,ts,f0,T,gamma,wallsize=5):
     n = int((tf-t0)/ts)+1
     t = np.linspace(t0,tf,n)
     holding = np.zeros((2, n))
-
     holding[:, 0] = f0
     fi = np.zeros((2))
-
+    tr = []
     for i in range(len(t) - 1):
+        if holding[0][i] >= wallsize or holding[0][i] <= -wallsize:
+            holding = holding[:, :i+1]
+            t = t[:i+1]
+            break
         fi = holding[:, i]
         ti = t[i]
         holding[:, i + 1] = ts*np.array(function(ti,fi,T,ts,gamma)) + fi #increments the function
+        tr.append(ti)
     return t, holding
 
 def langevin(t,val,T,ts,gamma):
-    
+    m = 1    
     #x = val[0]
     v = val[1]
     stdv = np.sqrt(2*gamma*T*ts)
@@ -32,7 +36,20 @@ def langevin(t,val,T,ts,gamma):
 
 def plotdata(t,f):
     plt.plot(t,f[0])
+    plt.plot(t,f[1])
     plt.savefig("trajectory.png")
+    plt.clf()
+
+def plothistogram(T,tf,ts,x0,v0,gamma,wallsize=5):
+    runs = 100
+    times = []
+    for i in range(runs):
+        t,f = euler(langevin,0,tf,ts,[x0,v0],T,gamma)
+        if f[0][-1] >= wallsize or f[0][-1] <= -wallsize:
+            times.append(t[-1])
+    plt.hist(times)
+    plt.savefig("histogram.png")
+
 
 def fileprint(t,f):
     ind = np.arange(len(t))
@@ -57,8 +74,11 @@ def getargs():
     gamma = args.damping_coefficient
     return T,tf,ts,x0,v0,gamma
 
-T,tf,ts,x0,v0,gamma = getargs()
-print(T,tf,ts,x0,v0,gamma)
-t,f = euler(langevin,0,tf,ts,[x0,v0])
-fileprint(t,f)
-plotdata(t,f)
+def main():
+    T,tf,ts,x0,v0,gamma = getargs()
+    t,f = euler(langevin,0,tf,ts,[x0,v0],T,gamma)
+    fileprint(t,f)
+    plotdata(t,f)
+    plothistogram(T,tf,ts,x0,v0,gamma)
+
+main()
