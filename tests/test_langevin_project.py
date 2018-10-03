ac7586ac9cdcd722459bdba366865e2780617d68
Jsavino <jsavino@u.rochester.edu>
2018-10-03 19:56:00 -0400

    Added comments to tests - these aren't as in depth, but do detail what is being tested

--- tests/test_langevin_project.py
+++ tests/test_langevin_project.py
@@ -28,12 +28,15 @@ def test_content(response):
 
 def test_euler():
 	t,f,hitwall = langevin_project.euler(langevin_project.langevin,0,10,0.1,[0,0],300,0.1,wallsize=5)
-	assert len(t) == len(f[0])
+	assert len(t) == len(f[0]) #tests that the length of the time array is the same as the position array
 
 def test_langevin():
+	np.random.seed(42)
 	dxdt,dvdt = langevin_project.langevin(0,[0.0,1.0],300,0.1,0.1)
+	#makes sure the values are type float, as passed into the function
 	assert type(dxdt) == float
 	assert type(dvdt) == float
+	#asserts that acceleration is not equal to the negative of velocity (due to random force)
 	bool = (dvdt != -dxdt*0.1)
 	assert bool
 
@@ -43,7 +46,7 @@ def test_plot():
 		os.remove('trajectory.png')
 	t,f,hitwall = langevin_project.euler(langevin_project.langevin,0,10,0.1,[0,0],300,0.1,wallsize=5)
 	langevin_project.plotdata(t,f)
-	assert os.path.exists('trajectory.png')
+	assert os.path.exists('trajectory.png') #makes sure the file is created
 
 def test_trials():
 	if os.path.exists('trajectory.png') and os.path.exists('histogram.png') and os.path.exists('output.txt'):
@@ -51,6 +54,7 @@ def test_trials():
 		os.remove('histogram.png')
 		os.remove('output.txt')
 	langevin_project.runtrials(300,10,0.1,0,0,0.1,wallsize=5,runs=100)
+        #makes sure all files are created
 	assert os.path.exists('histogram.png')
 	assert os.path.exists('trajectory.png')
 	assert os.path.exists('output.txt')
@@ -62,10 +66,11 @@ def test_write():
 	langevin_project.fileprint(t,f)	
 	a = np.loadtxt('output.txt', delimiter=',')
 	assert os.path.exists('output.txt')
-	assert a[-1][0] == len(t)-1
+	assert a[-1][0] == len(t)-1 #makes sure the output.txt is the same length as the time array
 
 def test_args():
 	T,tf,ts,x0,v0,gamma = langevin_project.getargs()
+	#checks to see if default arguments are passed correctly
 	assert T == 300
 	assert ts == 0.1
 
@@ -75,6 +80,7 @@ def test_main():
 		os.remove('histogram.png')
 		os.remove('output.txt')
 	langevin_project.main()
+	#makes sure files are created
 	assert os.path.exists('trajectory.png')
 	assert os.path.exists('output.txt')
 	assert os.path.exists('histogram.png')
